fbb1a41042fd5b177774b64d9c1bc12428d93ca9
andrew <Andrei.Grini@avanssoma.no>
2021-07-01 22:46:53 +0300

    database connected

--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@ https://www.youtube.com/watch?v=dDeWWQWMM-Y
 first we should install nest-cli
 sudo npm i -g @nestjs/cli
 
+npm install --save @nestjs/sequelize sequelize sequelize-typescript
+npm install --save pg pg-hstore
+npm install --save-dev @types/sequelize
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller'
+import { AppService } from './app.service';
 
 @Module({
     controllers: [AppController],
-    providers: []
+    providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'grini',
+            database: 'ulbi_nestjs',
+            models: [],
+            autoLoadModels: true,
+        }),
+    ],
 })
 export class AppModule {}
