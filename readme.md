34d46d1e7fbe23fec5f80d33630be0944af7605a
andrew <Andrei.Grini@avanssoma.no>
2021-07-15 22:54:50 +0300

    swagger installation

--- readme.md
+++ readme.md
@@ -2,10 +2,21 @@
 https://www.youtube.com/watch?v=dDeWWQWMM-Y
 ***
 
+nest new <project-name>
+
 first we should install nest-cli
 sudo npm i -g @nestjs/cli
 
 npm install --save @nestjs/sequelize sequelize sequelize-typescript
 npm install --save pg pg-hstore
 npm install --save-dev @types/sequelize
+npm i @nestjs/config
+npm i cross-env
+
+nest generate module users
+nest generate controller users
+nest generate service users
+
+
+ npm i @nestjs/swagger swagger-ui-express
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,28 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { AppController } from './app.controller'
-import { AppService } from './app.service';
+import { User } from './users/users.model';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
+        ConfigModule.forRoot({
+            // envFilePath: '.env',
+            envFilePath: `${process.env.NODE_ENV}`,
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'grini',
-            database: 'ulbi_nestjs',
-            models: [],
+            host: process.env.POSTGRES_HOST,
+            port: +process.env.POSTGRES_PORT,
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [User],
             autoLoadModels: true,
         }),
+        UsersModule,
     ],
 })
 export class AppModule {}
